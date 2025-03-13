42471f3ea9b133ba9f86ff2a530e83401a49a57a
Farizi <farizif@gmail.com>
2025-03-13 16:45:56 +0800

    feat(17-custom-actions): install missing packages

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -3,5 +3,14 @@ const core = require('@actions/core');
 async function run() { 
   core.info('I am a custom JS action');
 }
+/*
+1. Parse inputs:
+    1.1  base-branch from which to check for updates
+    1.2 target branch to use to create the PR
+    1.3 GitHub Token for authentication purposes (to create PRs)
+    1.4 Working directory for which to check for dependencies
+2. Execute NPM update command within the working directory
 
-run();
+*/
+
+run(); 
