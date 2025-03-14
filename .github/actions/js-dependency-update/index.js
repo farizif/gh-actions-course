5922158987efbd3f442eb3c2b51a7db5136e96da
Farizi <farizif@gmail.com>
2025-03-14 16:35:44 +0800

    feat(18-custom-actions): add action.yaml and dockerfile

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,18 +1,23 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
+const github = require('@actions/github');
 
 const validateBranchName = ({ branchName }) =>
     /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
   const validateDirectoryName = ({ dirName }) =>
     /^[a-zA-Z0-9_\-\/]+$/.test(dirName);
 
-async function run() { 
-    const baseBranch = core.getInput ('base-branch');
-    const targetBranch = core.getInput ('target-branch');
-    const ghToken = core.getInput ('gh-token');
-    const workingDir = core.getInput ('working-directory');
-    const debug = core.getBooleanInput ('debug');
+  async function run() { 
+  const baseBranch = core.getInput('base-branch', { required: true });
+  const headBranch = core.getInput('head-branch', { required: true });
+  const ghToken = core.getInput('gh-token', { required: true });
+  const workingDir = core.getInput('working-directory', { required: true });
+  const debug = core.getBooleanInput('debug');
 
-    core.setSecret(ghToken);
+  const commonExecOpts = {
+    cwd: workingDir,
+  };
+  core.setSecret(ghToken);
 
     if (!validateBranchName({ branchName: baseBranch })) {
         core.setFailed(
@@ -21,9 +26,9 @@ async function run() {
         return;
       }
     
-      if (!validateBranchName({ branchName: targetBranch })) {
+      if (!validateBranchName({ branchName: headBranch })) {
         core.setFailed(
-          'Invalid target-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots, and forward slashes.'
+          'Invalid head-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots, and forward slashes.'
         );
         return;
       }
@@ -35,27 +40,56 @@ async function run() {
         return;
       }
     
-      core.info(`[js-dependency-update] : base branch is ${baseBranch}`);
-      core.info(`[js-dependency-update] : target branch is ${targetBranch}`);
+      core.info(`[js-dependency-update] : base branch is ${baseBranch}`); 
+      core.info(`[js-dependency-update] : head branch is ${headBranch}`);
       core.info(`[js-dependency-update] : working directory is ${workingDir}`);
     
       await exec.exec('npm update', [], {
         cwd: workingDir,
-      });
+      }); 
     
       const gitStatus = await exec.getExecOutput(
         'git status -s package*.json',
         [],
         {
-          cwd: workingDir,
+          ...commonExecOpts,
         }
       );
-    
-      if (gitStatus.stdout.length > 0) {
-        core.info('[js-dependency-update] : There are updates available!');
-      } else {
-        core.info('[js-dependency-update] : No updates at this point in time.');
-      }
+
+  if (gitStatus.stdout.length > 0) {
+    core.info('[js-dependency-update] : There are updates available!');
+    await exec.exec(`git config --global user.name "gh-automation"`);
+    await exec.exec(`git config --global user.email "gh-automation@email.com"`);
+    await exec.exec(`git checkout -b ${headBranch}`, [], {
+      ...commonExecOpts,
+    });
+    await exec.exec(`git add package.json package-lock.json`, [], {
+      ...commonExecOpts,
+    });
+    await exec.exec(`git commit -m "chore: update dependencies`, [], {
+      ...commonExecOpts,
+    });
+    await exec.exec(`git push -u origin ${headBranch} --force`, [], {
+      ...commonExecOpts,
+    });
+
+    try {
+      await octokit.rest.pulls.create({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        title: `Update NPM dependencies`,
+        body: `This pull request updates NPM packages`,
+        base: baseBranch,
+        head: headBranch
+      });
+    } catch (e) {
+      core.error('[js-dependency-update] : Something went wrong while creating the PR. Check logs below.')
+      core.setFailed(e.message);
+      core.error(e);
+    }
+  } else {
+    core.info('[js-dependency-update] : No updates at this point in time.');
+  }
 
 /*
 1. Parse inputs:
@@ -71,5 +105,4 @@ async function run() {
 5. Otherwise, conclude the custom action
 */
 
-run();
-    }
+run();}
